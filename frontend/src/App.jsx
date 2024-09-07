66fdf8036103659b89d3278749132743cde91f3f
Ashvatth Joshi <codingash27@gmail.com>
2024-09-07 16:11:17 +0530

    fixed react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Input from './components/Input'
+import List from './components/List'
+import './components/Style.css'
+
+export default function App() {
+    return (
+        <div>
+            <Input />
+            <List />
+        </div>
+    )
+}
